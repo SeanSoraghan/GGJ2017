4094cb644ba1d12783cedb4f6a22ef1b6a9a5379
Federico Fasce <federico@kurai.eu>
2017-01-21 20:36:18 +0000

    Flower Scene done, Fish started prototyping

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour {
+	public Transform fishTarget;
+	public Transform otherTarget;
+	private Transform perceivedTarget;
+	// Use this for initialization
+	void Start () {
+		perceivedTarget = fishTarget;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = (perceivedTarget.position - transform.position);
+		float angle = (Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg)-90;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.position += transform.up * Time.deltaTime;
+
+		if (Input.anyKeyDown)
+		{
+			perceivedTarget = otherTarget;
+		}
+	}
+}
---
+++ Assets/Scripts/FlowerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerController : MonoBehaviour {
+	public Transform[] targetAngles;
+	public Transform[] petals;
+	public Quaternion[] petalsInitialRotations;
+	public BeeMovement bee;
+
+	public float speedOpening=0.4f;
+	public float speedClosing=1;
+
+	float interpolator;
+	// Use this for initialization
+	void Start () {
+		interpolator = 0;
+		petalsInitialRotations = new Quaternion [petals.Length];
+		for (int i=0; i<petals.Length; i++)
+		{
+			petalsInitialRotations [i] = petals [i].rotation;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Debug.Log (interpolator);
+
+		if (interpolator<=1)
+		{
+			if (Input.GetKey ("a"))
+				interpolator += Time.deltaTime*speedOpening;
+			else
+			{
+				interpolator = Mathf.Clamp (interpolator - Time.deltaTime*speedClosing, 0, 1);
+			}
+
+			for (int i=0; i<petals.Length; i++)
+			{
+				petals [i].rotation = Quaternion.Slerp (petalsInitialRotations[i], targetAngles [i].rotation, interpolator);
+			}
+		}
+
+		else
+		{
+			if (bee.enabled==false)
+			{
+				bee.enabled = true;
+			}
+		}
+
+	}
+}
