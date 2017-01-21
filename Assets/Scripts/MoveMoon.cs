477e4d342c18539bc6889115dcae2a2a857b903d
Wolfie\Tim <timphillips7688@msn.com>
2017-01-21 22:25:54 +0100

    implemented moon scene

---
+++ Assets/Scripts/MoveMoon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMoon : MonoBehaviour {
+
+    public float moveSpeed = 0.5f;
+    public float returnSpeed = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+       
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            gameObject.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            Debug.Log(transform.position.x);
+        }
+        else
+        {
+            if (gameObject.transform.localPosition.x < 0.01)
+            {
+                gameObject.transform.Translate(Vector3.right * returnSpeed * Time.deltaTime);
+            }
+        }
+
+	}
+}
---
+++ Assets/Scripts/MoveWaves.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWaves : MonoBehaviour {
+
+    public float speed;
+    public float spriteWidth;
+    private float screenWidth;
+    private float trainLength;
+    public float numberOfClones;
+
+
+	// Use this for initialization
+	void Start () {
+
+        screenWidth = Screen.width;
+        spriteWidth = gameObject.GetComponent<SpriteRenderer>().sprite.rect.width;
+        trainLength =  numberOfClones * (spriteWidth/100);
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.Translate(Vector3.right* speed * Time.deltaTime);
+
+        if (gameObject.transform.position.x > 30)
+            {
+                
+                gameObject.transform.Translate(-trainLength, 0, 0);
+            }
+		
+	}
+}
